99279303d34574c0f766f58f3195269594e19b03
Aleksei Gusev <aleksei.gusev@gmail.com>
2012-12-26 22:14:40 +0300

    Generate debian/ with dh_make.

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(plasma-runner-cliprunner):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="plasma-runner-cliprunner" command="/usr/bin/plasma-runner-cliprunner"
---
+++ debian/plasma-runner-cliprunner.default.ex
@@ -0,0 +1,10 @@
+# Defaults for plasma-runner-cliprunner initscript
+# sourced by /etc/init.d/plasma-runner-cliprunner
+# installed at /etc/default/plasma-runner-cliprunner by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
