b2ef6188926d1d7a1b7d9a95bfbe5d3593c1f460
Aleksei Gusev <aleksei.gusev@gmail.com>
2012-12-27 21:33:28 +0300

    Remove examples from debian/.

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(plasma-runner-cliprunner):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="plasma-runner-cliprunner" command="/usr/bin/plasma-runner-cliprunner"
--- debian/plasma-runner-cliprunner.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the plasma-runner-cliprunner package
-#
-0 4	* * *	root	[ -x /usr/bin/plasma-runner-cliprunner_maintenance ] && /usr/bin/plasma-runner-cliprunner_maintenance
